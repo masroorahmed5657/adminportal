5cbb747e722a5f03939fc579f37a189d0b2651d2
ukasha khursheed <ukashakhursheed@gmail.com>
2026-07-22 16:47:44 +0500

    Minor formatting fix in layout.component.spec.ts

--- src/app/layouts/layout/layout.component.spec.ts
+++ src/app/layouts/layout/layout.component.spec.ts
@@ -10,7 +10,7 @@ describe('LayoutComponent', () => {
     await TestBed.configureTestingModule({
       imports: [LayoutComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(LayoutComponent);
     component = fixture.componentInstance;
